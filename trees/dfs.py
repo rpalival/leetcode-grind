e7a328d7a8fbad4cb39d9c6fef3583e2b1e89fd0
Raj Palival <rajpalival21@gmail.com>
2024-10-26 20:52:05 -0400

    BST DFS

---
+++ trees/dfs.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+    
+def inorder(root):
+    if not root:
+        return
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+def revorder(root):
+    if not root:
+        return
+    revorder(root.right)
+    print(root.val)
+    revorder(root.left)
+
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
